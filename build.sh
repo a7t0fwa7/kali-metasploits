b115083b2eaa0327be5ee60a8b08ce7bcf7d5340
rsundahl <roy@polyverse.io>
2017-10-26 12:45:46 -0700

    Cleaned up for consistency with tcp_echo_server.

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
 
-pv build $1 docker
-pv build -r polyverse-internal.jfrog.io $1 docker
+build()
+{
+	command="pv build ${@:1} docker"
+	echo $command
+	$command
+}
+
+# Special keyword "all" means build and push to both polyverse and jfrog repos
+if [ "$1" == "all" ]; then
+	build -s -r polyverse
+	build -s -r internal.hub.polyverse.io
+else
+	build "$@"
+fi
