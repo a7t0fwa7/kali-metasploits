9c0fe0aec378bd4bed6b03011d8d1a9239571a6c
rsundahl <roy@polyverse.com>
2020-03-03 11:57:08 -0800

    Tighten dependencies before mining for gadgets.

--- tcp-echo-server.sh
+++ tcp-echo-server.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 declare -r loc=kali-metasploit
-declare -r rem=tcp-echo-server.xenial
+declare -r rem=tcp-echo-server.alpine
 
 # Get rid of any hangers-on
 docker rm -f $loc $rem 2>/dev/null
 
 # Start the remote target and get its IP address
-docker run -dt --rm --name $rem polyverse/$rem
+docker run -dt --rm --name $rem polyverse/$rem:4079c32c8cc563d7acb0644178c286c4dbc23fb1
 RHOST=$(docker exec -it $rem tail -n1 /etc/hosts | awk '{print $1}')
 
 # Get the IP address of the attacker (that we will hopefully get again)
