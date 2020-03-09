5d2c114026092f8979a7d45ad48ddbcf528fbc3b
rsundahl <roy@polyverse.com>
2020-03-09 15:21:42 -0700

    Lines should not have been commented out.

--- readhook-node-echo-server.sh
+++ readhook-node-echo-server.sh
@@ -6,10 +6,10 @@ declare -r rem=readhook-node-echo-server
 declare -r sha=30ccb08b07302bc1a36e16b3587d52fd5c8f9d33 # readhook-node-echo-server from jitrop branch
 
 # Get rid of any hangers-on
-#docker rm -f $loc $rem 2>/dev/null
+docker rm -f $loc $rem 2>/dev/null
 
 # Start the remote target and get its IP address
-#docker run --privileged -dt --rm --name $rem polyverse/$rem:$sha
+docker run --privileged -dt --rm --name $rem polyverse/$rem:$sha
 RHOST=$(docker exec -it $rem tail -n1 /etc/hosts | awk '{print $1}')
 
 # Get the IP address of the attacker (that we will hopefully get again)
